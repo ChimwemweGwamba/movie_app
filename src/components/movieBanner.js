b1f738887a3eb2cbdd588fa624d16df2640bfa74
ChimwemweGwamba <chimwemwebengist@gmail.com>
2023-02-05 20:43:34 +0200

    created similar Movies component and it was added to viewMovie page

--- src/components/movieBanner.js
+++ src/components/movieBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { device } from "../data/devices";
 import Rating from "./rating";
+import SimilarMovies from "./similarMovies";
 
 function MovieBanner(props) {
   return (
@@ -10,49 +11,60 @@ function MovieBanner(props) {
         url: "https://image.tmdb.org/t/p/original" + props.movie.backdrop_path,
       }}
     >
-      <ImageContainer>
-        <MovieImage
-          src={"https://image.tmdb.org/t/p/w500" + props.movie.poster_path}
-        ></MovieImage>
-      </ImageContainer>
+      <TopSection>
+        <ImageContainer>
+          <MovieImage
+            src={"https://image.tmdb.org/t/p/w500" + props.movie.poster_path}
+          ></MovieImage>
+        </ImageContainer>
 
-      <TextContainer>
-        <MovieTitle>{`${props.movie.title} (${props.movie.release_date})`}</MovieTitle>
-        <MovieDesc>{props.movie.overview}</MovieDesc>
-        <Rating
-          ratings={Math.round(props.movie.vote_average * 10)}
-          ratingsColor={
-            props.movie.vote_average >= 8.0
-              ? "green"
-              : props.movie.vote_average >= 6.5
-              ? "orange"
-              : "red"
-          }
-        />
-      </TextContainer>
+        <TextContainer>
+          <MovieTitle>{`${props.movie.title} (${props.movie.release_date})`}</MovieTitle>
+          <MovieDesc>{props.movie.overview}</MovieDesc>
+          <Rating
+            ratings={Math.round(props.movie.vote_average * 10)}
+            ratingsColor={
+              props.movie.vote_average >= 8.0
+                ? "green"
+                : props.movie.vote_average >= 6.5
+                ? "orange"
+                : "red"
+            }
+          />
+        </TextContainer>
+      </TopSection>
+
+      <SimilarMoviesContainer>
+        <SimilarMovies />
+      </SimilarMoviesContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
-  height: 95vh;
   width: 100%;
+  height: auto;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+
   background-image: url("${(props) => props.backgroundImg.url}");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
-
   background-color: #393939;
   background-blend-mode: multiply;
 
   @media ${device.desktop} {
-    height: 95vh;
-    flex-direction: row;
-    justify-content: space-around;
   }
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 100px;
 `;
 
 const ImageContainer = styled.div`
@@ -93,6 +105,11 @@ const MovieDesc = styled.p`
 
   @media ${device.desktop} {
     text-align: left;
+  }
+`;
+
+const SimilarMoviesContainer = styled.div`
+  @media ${device.desktop} {
   }
 `;
 
